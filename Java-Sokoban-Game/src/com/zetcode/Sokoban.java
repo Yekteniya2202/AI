228fd4c0493cf4d3d01b8a537cbe2e9980831355
Yekteniya2202 <yellowblue924@yandex.ru>
2022-10-05 01:16:50 +0300

    modified lab 1

--- Java-Sokoban-Game/src/com/zetcode/Sokoban.java
+++ Java-Sokoban-Game/src/com/zetcode/Sokoban.java
@@ -14,23 +14,33 @@ public class Sokoban extends JFrame {
     }
 
     private void initUI() {
-        
+
         Board board = new Board();
         add(board);
         StateSpace stateSpace = new StateSpace(board);
+        long time = System.currentTimeMillis();
         Stack<Integer> solutionStack = stateSpace.getSolution();
-        System.out.println(solutionStack);
+        System.out.println("RESULTS");
+        System.out.println("Finished for = " + (System.currentTimeMillis() - time) + " millis");
+        System.out.println("Iterations = " + stateSpace.iterations);
+        System.out.println("Steps count = " + solutionStack.size());
+        System.out.println("Revealing count = " + stateSpace.revealingCount);
+        System.out.println("Revealed states = " + stateSpace.revealedStatesCount);
+        System.out.println("Max states count = " + stateSpace.maxStatesCount);
+        System.out.println("Max opened states = " + stateSpace.maxOpenCount);
+        System.out.println("Max closed states = " + stateSpace.maxCloseCount);
+
         setTitle("Sokoban");
         board.addSolution(solutionStack);
         setSize(board.getBoardWidth() + OFFSET,
                 board.getBoardHeight() + 2 * OFFSET);
-        
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
     }
 
     public static void main(String[] args) {
-        
+
         EventQueue.invokeLater(() -> {
 
             Sokoban game = new Sokoban();
--- Java-Sokoban-Game/src/com/zetcode/StateSpace.java
+++ Java-Sokoban-Game/src/com/zetcode/StateSpace.java
@@ -4,15 +4,35 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.function.Predicate;
 
+/*
+кол-во итераций цикла поиска +
+кол-во операций раскрытия узлов +
+кол-во раскрываний +
+кол-во раскрытых на итерации +
+время поиска (мс) (пропорционально итераций)
+кол-во шагов в решении
+максимальный объем памяти (кол-во узлов)
+
+
+ */
 public class StateSpace {
     private Board board;
 
-    private Stack<State> open = new Stack<>();
+    private Queue<State> open = new ArrayDeque<>();
 
     private Set<State> openSet = new HashSet<>(10000000);
     private Set<State> closed = new HashSet<>(10000000);
 
     private Stack<Integer> solutionPath = new Stack<>();
+
+
+    public int iterations = 0;
+    public int revealingCount = 0;
+    public int revealedStatesCount = 0;
+    public int maxStatesCount = 0;
+
+    public int maxOpenCount = 0;
+    public int maxCloseCount = 0;
 
     public StateSpace(Board board) {
         this.board = board;
@@ -25,12 +45,24 @@ public class StateSpace {
         openSet.add(initialState);
 
         while (open.size() != 0) {
-            State state = open.pop();
+            iterations++;
+            if (open.size() > maxOpenCount){
+                maxOpenCount = open.size();
+            }
+
+            int sum = open.size() + closed.size();
+            if (sum > maxStatesCount){
+                maxStatesCount = sum;
+            }
+
+
+            State state = open.poll();
 
             if (state.isCompleted()) {
                 board.setState(initialState);
                 System.out.println(closed.size());
                 System.out.println(state);
+                maxCloseCount = closed.size();
                 return state.getSolutionPath(solutionPath);
             }
 
@@ -38,11 +70,13 @@ public class StateSpace {
             openSet.remove(state);
 
             List<State> revealedStates = state.revealState();
+            revealingCount++;
+            revealedStatesCount += revealedStates.size();
 
             for (State childState : revealedStates) {
 
                 if (!openSet.contains(childState) && !closed.contains(childState)) {
-                    open.push(childState);
+                    open.add(childState);
                     openSet.add(childState);
                 }
             }
